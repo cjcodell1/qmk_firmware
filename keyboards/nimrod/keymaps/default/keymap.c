a136f2328e748ec98317d43eda034c91dd85a232
Carter Codell <cjcodell1@gmail.com>
2020-12-05 12:28:10 -0500

    nimrod done

--- keyboards/nimrod/keymaps/default/keymap.c
+++ keyboards/nimrod/keymaps/default/keymap.c
@@ -17,37 +17,44 @@
 
 enum layers{
   _BASE,
-  _LOWER,
-  _RAISE,
-  _ADJUST
+  _NUM_SYM
 };
+
+enum combo_events {
+  COMBO_TAB,
+  COMBO_ESC,
+  COMBO_DEL,
+};
+
+#define KC_SF LSFT_T(KC_F)
+#define KC_SJ RSFT_T(KC_J)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_grid(
-  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,            KC_Y,   KC_U,       KC_I,    KC_O,   KC_P,
-  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,            KC_H,   KC_J,       KC_K,    KC_L,   KC_ENT,
-  KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,            KC_N,   KC_M,       KC_COMM, KC_DOT, KC_SLSH,
-  KC_LCTL, KC_LALT, KC_LGUI, MO(_LOWER), LSFT_T(KC_BSPC), KC_SPC, MO(_RAISE), KC_ESC,  KC_TAB, KC_DEL
+  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,
+  KC_A,    KC_S,    KC_D,    KC_SF,   KC_G,         KC_H,   KC_SJ,   KC_K,    KC_L,    KC_ENT,
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+  KC_LCTL, KC_LALT, KC_LGUI, KC_BSPC, MO(_NUM_SYM), KC_SPC, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT
   ),
 
-  [_LOWER] = LAYOUT_grid(
-  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,     KC_ASTR, KC_LPRN, KC_RPRN,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,   KC_LCBR, KC_RCBR,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,   KC_HOME, KC_END,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_ADJUST), KC_TRNS, KC_TRNS, KC_TRNS
-  ),
-
-  [_RAISE] = LAYOUT_grid(
-  KC_1,    KC_2,    KC_3,    KC_4,        KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-  KC_GRV,  KC_NO,   KC_NO,   KC_BSLS,     KC_QUOT, KC_SCLN, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGUP, KC_PGDN,
-  KC_TRNS, KC_TRNS, KC_TRNS, MO(_ADJUST), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-  ),
-
-  [_ADJUST] = LAYOUT_grid(
-  RESET,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_UP,   KC_NO,    KC_NO,
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO,
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_NO,    KC_NO,
+  [_NUM_SYM] = LAYOUT_grid(
+  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0,
+  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_EQUAL, KC_MINS,
+  KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS, KC_RPRN, KC_RBRC, KC_RCBR, KC_SCLN, KC_QUOTE, KC_BSLASH
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS
   ),
+
 };
+
+#ifdef COMBO_ENABLE
+const uint16_t PROGMEM combo_tab[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM combo_esc[] = {KC_E, KC_W, COMBO_END};
+const uint16_t PROGMEM combo_del[] = {KC_MINS, KC_EQL, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+  [COMBO_TAB] = COMBO(combo_tab,KC_TAB),
+  [COMBO_ESC] = COMBO(combo_esc,KC_ESC),
+  [COMBO_DEL] = COMBO(combo_del,KC_DEL),
+
+};
+#endif
--- keyboards/nimrod/nimrod.h
+++ keyboards/nimrod/nimrod.h
@@ -32,19 +32,6 @@
   { K30, K31, K32, K33, K34, K35, K36, K37, K38, K39  }  \
 }
 
-#define LAYOUT_right_space( \
-  K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, \
-  K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, \
-  K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
-  K30, K31, K32, K33, K34,           K37            \
-) \
-{ \
-  { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09  }, \
-  { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19  }, \
-  { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29  }, \
-  { K30, K31, K32, K33, K34, XXX, XXX, K37, XXX, XXX  }  \
-}
-
 #define LAYOUT_left_space( \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, \
   K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, \
@@ -56,6 +43,32 @@
   { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19  }, \
   { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29  }, \
   { XXX, XXX, K32, XXX, XXX, K35, K36, K37, K38, K39  }  \
+}
+
+#define LAYOUT_center_space( \
+  K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, \
+  K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, \
+  K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
+  K30, K31,           K34,           K37, K38, K39  \
+) \
+{ \
+  { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09  }, \
+  { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19  }, \
+  { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29  }, \
+  { K30, K31, XXX, XXX, K34, XXX, XXX, K37, K38, K39  }  \
+}
+
+#define LAYOUT_right_space( \
+  K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, \
+  K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, \
+  K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
+  K30, K31, K32, K33, K34,           K37            \
+) \
+{ \
+  { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09  }, \
+  { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19  }, \
+  { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29  }, \
+  { K30, K31, K32, K33, K34, XXX, XXX, K37, XXX, XXX  }  \
 }
 
 #define LAYOUT_split_space( \
